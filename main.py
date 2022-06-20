b205c38e89773052bccd2f1629d7ba8113fe74c4
Amy Rivera <107879546+AR1190@users.noreply.github.com>
2022-06-20 19:03:32 +0000

    Madlibs

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ userAge = 2022 - userYear
 print(str(userAge) + " is so young!")
 '''
 
+# Create your own Madlibs
+# Have at least 5 inputs
+
+userName = input("Please give me a person's name: ")
+print("username " + "was taking a nap")
+adjective = input("Please give me a adjective: ")
